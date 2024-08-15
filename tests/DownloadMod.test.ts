3cebe278cc468e4a998d8f01481546e3215c50aa
The_Gost_sniper <54660359+TheGostsniperfr@users.noreply.github.com>
2024-08-15 10:58:29 +0200

    ✅ Add Overwriting test

--- tests/DownloadMod.test.ts
+++ tests/DownloadMod.test.ts
@@ -76,3 +76,53 @@ describe("Download Mekanism mod", () => {
         }
     }, 60 * 1000); 
 });
+
+describe("Overwriting test", () => {
+    const oldModFile = new ModFile(
+        "EnderIO-1.20.1.jar", //Specific version deliberately not specified for this test
+        "ccd574b455859801058799e10ec8c2aca4060f0f",
+        HashTypes.SHA1,
+        "https://edge.forgecdn.net/files/5460/93/EnderIO-1.20.1-6.1.2-beta-all.jar"
+    );
+
+    const newModFile = new ModFile(
+        "EnderIO-1.20.1.jar", //Specific version deliberately not specified for this test
+        "7d8a8fb4fc58839ef56336fa998d82f312367f1d",
+        HashTypes.SHA1,
+        "https://edge.forgecdn.net/files/5549/669/EnderIO-1.20.1-6.1.8-beta-all.jar"
+    );
+
+    const modsDir = path.join("./.minecraft", "mods");
+    const oldFilePath = path.join(modsDir, oldModFile.getFileName());
+    const newFilePath = path.join(modsDir, newModFile.getFileName());
+
+    beforeAll(() => {
+        deleteFileIfExists(oldFilePath);
+        deleteFileIfExists(newFilePath);
+    });
+
+    afterAll(() => {
+        deleteFileIfExists(oldFilePath);
+        deleteFileIfExists(newFilePath);
+    })
+
+    it("Should download and overwrite the EnderIO jar file", async () => {        
+        createFolderIfNotExist(modsDir);
+
+        try {
+            // First download
+            await oldModFile.update(modsDir, true);
+            let currentFileHash = await hashFile(oldFilePath, HashTypes.SHA1);
+            expect(currentFileHash).toEqual(oldModFile.getHash());
+
+            // Second download (to test overwrite)
+            await newModFile.update(modsDir, true);
+            currentFileHash = await hashFile(newFilePath, HashTypes.SHA1);
+            expect(currentFileHash).toEqual(newModFile.getHash());
+
+        } catch (error) {
+            console.error('Test failed with error:', error);
+            throw error;
+        }
+    }, 60 * 1000); 
+});
